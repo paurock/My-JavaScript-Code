a747c18615693f2b1ca5e948f008a5e8e689d3df
paurock <38553025+paurock@users.noreply.github.com>
2018-08-18 13:15:53 +0300

    Create myTimer.js

---
+++ myTimer.js
@@ -0,0 +1,28 @@
+const interval = () => 1000
+const currentDate = () => new Date()
+const log = (result) => console.log(result)
+const clear = () => console.clear()
+			
+const normalDate = (currentDate) => ({				
+	hours:  currentDate.getHours(),
+	minutes:  currentDate.getMinutes(),
+	seconds:  currentDate.getSeconds()		
+})
+			
+const addZeros = key => clockTime =>
+	({
+		...clockTime,
+		[key]: (clockTime[key] < 10) ?
+		"0" + clockTime[key] :
+		clockTime[key]
+})
+
+							 
+const compose = arr => val => arr.reduce(	(fn1, fn2) => fn2(fn1), val)	
+const ddc = dubbleDigitClock = compose([addZeros("hours"), addZeros("minutes"), addZeros("seconds")])
+const readyClocks = ddc =>  `${ddc.hours}:${ddc.minutes}:${ddc.seconds}`		
+const showNormalDate = compose([clear, currentDate, normalDate, dubbleDigitClock, readyClocks, log])
+const tickingClock = () => setInterval(showNormalDate, interval())	
+
+//log( dubbleDigitClock(normalDate(currentDate())).hours )
+tickingClock()
