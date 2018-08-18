289415f97eb42587de0d171e8ba52a9e78238631
paurock <38553025+paurock@users.noreply.github.com>
2018-08-18 13:16:58 +0300

    Update myTimer.js

--- myTimer.js
+++ myTimer.js
@@ -18,11 +18,10 @@ const addZeros = key => clockTime =>
 })
 
 							 
-const compose = arr => val => arr.reduce(	(fn1, fn2) => fn2(fn1), val)	
+const compose = arr => val => arr.reduce((fn1, fn2) => fn2(fn1), val)	
 const ddc = dubbleDigitClock = compose([addZeros("hours"), addZeros("minutes"), addZeros("seconds")])
 const readyClocks = ddc =>  `${ddc.hours}:${ddc.minutes}:${ddc.seconds}`		
 const showNormalDate = compose([clear, currentDate, normalDate, dubbleDigitClock, readyClocks, log])
 const tickingClock = () => setInterval(showNormalDate, interval())	
 
-//log( dubbleDigitClock(normalDate(currentDate())).hours )
 tickingClock()
